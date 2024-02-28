cb0efb1d40d62e4d75532a8924d00eec3da6e66c
Vohraanik <anikvohra3@gmail.com>
2024-02-28 08:17:46 +0530

    29-02-2024

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React First time
+          Learn React 
         </a>
       </header>
     </div>
