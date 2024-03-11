d70322e8d489a4a13ab3050a77ab8d6613c4e222
Vohraanik <153048777+Vohraanik@users.noreply.github.com>
2024-03-11 07:53:33 +0530

    done

--- src/user/containers/loading/Loading.js
+++ src/user/containers/loading/Loading.js
@@ -2,18 +2,24 @@ import React, { useState, useEffect } from 'react';
 import Backdrop from '@mui/material/Backdrop';
 import CircularProgress from '@mui/material/CircularProgress';
 
-
 function Loading(props) {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
 
-    const getData = async () => {
 
-        const response = await fetch('https://type.fit/api/quotes');
-        const data = await response.json();
-        console.log(data);
-        setData(data);
+    const handleClose = () => {
         setLoading(false);
+    };
+
+
+    const getData = async () => {
+  
+            const response = await fetch('https://type.fit/api/quotes');
+            const data = await response.json();
+            setData(data);
+            setTimeout(()=>{
+                setLoading(false);
+            }, 3000);
     };
 
     useEffect(() => {
@@ -22,24 +28,22 @@ function Loading(props) {
 
     return (
         <div>
-            <Backdrop
-                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-                open={loading}
-
-            >
-                <CircularProgress color="inherit" />
-            </Backdrop>
-            {
-                data.map((v) => (
-                    <div>
+        { loading ? (
+                <Backdrop
+                    sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                    open={loading}
+                    onClick={handleClose}
+                >
+                    <CircularProgress color="inherit" />
+                </Backdrop>
+            ) : (
+                data.map((v, index) => (
+                    <div key={index}>
                         <h1>{v.text}</h1>
                         <h2>{v.author}</h2>
                     </div>
                 ))
-            }
-
-
-
+            )}
         </div>
     );
 }
